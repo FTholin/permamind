f8ccb61a0680f05b048f06f85c022647b7fdcb66
FTholin <ProteusIV@protonmail.com>
2019-04-03 01:30:08 +0200

    better bloc architecture / TODO permamind http respond

--- permamind_mobile/lib/blocs/bloc_provider.dart
+++ permamind_mobile/lib/blocs/bloc_provider.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+Type _typeOf<T>() => T;
+
 abstract class BlocBase {
   void dispose();
 }
@@ -11,30 +13,45 @@ class BlocProvider<T extends BlocBase> extends StatefulWidget {
     @required this.bloc,
   }): super(key: key);
 
-  final T bloc;
   final Widget child;
+  final T bloc;
 
   @override
   _BlocProviderState<T> createState() => _BlocProviderState<T>();
 
   static T of<T extends BlocBase>(BuildContext context){
-    final type = _typeOf<BlocProvider<T>>();
-    BlocProvider<T> provider = context.ancestorWidgetOfExactType(type);
-    return provider.bloc;
+    final type = _typeOf<_BlocProviderInherited<T>>();
+    _BlocProviderInherited<T> provider =
+        context.ancestorInheritedElementForWidgetOfExactType(type)?.widget;
+    return provider?.bloc;
   }
-
-  static Type _typeOf<T>() => T;
 }
 
-class _BlocProviderState<T> extends State<BlocProvider<BlocBase>>{
+class _BlocProviderState<T extends BlocBase> extends State<BlocProvider<T>>{
   @override
   void dispose(){
-    widget.bloc.dispose();
+    widget.bloc?.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context){
-    return widget.child;
+    return new _BlocProviderInherited<T>(
+      bloc: widget.bloc,
+      child: widget.child,
+    );
   }
+}
+
+class _BlocProviderInherited<T> extends InheritedWidget {
+  _BlocProviderInherited({
+    Key key,
+    @required Widget child,
+    @required this.bloc,
+  }) : super(key: key, child: child);
+
+  final T bloc;
+
+  @override
+  bool updateShouldNotify(_BlocProviderInherited oldWidget) => false;
 }
