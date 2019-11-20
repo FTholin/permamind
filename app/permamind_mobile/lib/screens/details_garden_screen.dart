8aa1d97fa397ddbd26464b0d75d6a290e429c325
Florian THOLIN <ProteusIV@protonmail.com>
2019-11-20 15:57:23 +0100

    start datetime integration for scheduler

--- app/permamind_mobile/lib/screens/details_garden_screen.dart
+++ app/permamind_mobile/lib/screens/details_garden_screen.dart
@@ -18,8 +18,6 @@ class DetailsGardenScreen extends StatelessWidget {
     return BlocBuilder<GardensBloc, GardensState>(
       builder: (BuildContext context, GardensState state) {
 
-        print("Garden id ${garden.id}");
-
         final localizations = ArchSampleLocalizations.of(context);
         return Scaffold(
           appBar: AppBar(
@@ -37,7 +35,7 @@ class DetailsGardenScreen extends StatelessWidget {
         ),
             ],
           ),
-          body: Scheduler(title: 'Table Calendar Demo')
+          body: Scheduler(garden: garden),
         );
       },
     );
@@ -56,15 +54,16 @@ final Map<DateTime, List> _holidays = {
 };
 
 class Scheduler extends StatefulWidget {
-  Scheduler({Key key, this.title}) : super(key: key);
+  Scheduler({Key key, this.garden}) : super(key: key);
 
-  final String title;
+  final Garden garden;
 
   @override
   _SchedulerState createState() => _SchedulerState();
 }
 
 class _SchedulerState extends State<Scheduler> with TickerProviderStateMixin {
+
   Map<DateTime, List> _events;
   List _selectedEvents;
   AnimationController _animationController;
@@ -76,22 +75,24 @@ class _SchedulerState extends State<Scheduler> with TickerProviderStateMixin {
     final _selectedDay = DateTime.now();
 
     _events = {
-      _selectedDay.subtract(Duration(days: 30)): ['Event A0', 'Event B0', 'Event C0'],
-      _selectedDay.subtract(Duration(days: 27)): ['Event A1'],
-      _selectedDay.subtract(Duration(days: 20)): ['Event A2', 'Event B2', 'Event C2', 'Event D2'],
-      _selectedDay.subtract(Duration(days: 16)): ['Event A3', 'Event B3'],
-      _selectedDay.subtract(Duration(days: 10)): ['Event A4', 'Event B4', 'Event C4'],
-      _selectedDay.subtract(Duration(days: 4)): ['Event A5', 'Event B5', 'Event C5'],
-      _selectedDay.subtract(Duration(days: 2)): ['Event A6', 'Event B6'],
-      _selectedDay: ['Event A7', 'Event B7', 'Event C7', 'Event D7'],
-      _selectedDay.add(Duration(days: 1)): ['Event A8', 'Event B8', 'Event C8', 'Event D8'],
-      _selectedDay.add(Duration(days: 3)): Set.from(['Event A9', 'Event A9', 'Event B9']).toList(),
-      _selectedDay.add(Duration(days: 7)): ['Event A10', 'Event B10', 'Event C10'],
-      _selectedDay.add(Duration(days: 11)): ['Event A11', 'Event B11'],
-      _selectedDay.add(Duration(days: 17)): ['Event A12', 'Event B12', 'Event C12', 'Event D12'],
-      _selectedDay.add(Duration(days: 22)): ['Event A13', 'Event B13'],
-      _selectedDay.add(Duration(days: 26)): ['Event A14', 'Event B14', 'Event C14'],
+      _selectedDay.add(Duration(days: 1)): ['Event A12', 'Event B12', 'Event C12', 'Event D12'],
+
+//      _selectedDay.subtract(Duration(days: 30)): ['Event A0', 'Event B0', 'Event C0'],
+//      _selectedDay.subtract(Duration(days: 27)): ['Event A1'],
+//      _selectedDay.subtract(Duration(days: 20)): ['Event A2', 'Event B2', 'Event C2', 'Event D2'],
+//      _selectedDay.subtract(Duration(days: 16)): ['Event A3', 'Event B3'],
+//      _selectedDay.subtract(Duration(days: 10)): ['Event A4', 'Event B4', 'Event C4'],
+//      _selectedDay.subtract(Duration(days: 4)): ['Event A5', 'Event B5', 'Event C5'],
+//      _selectedDay.subtract(Duration(days: 2)): ['Event A6', 'Event B6'],
+//      _selectedDay: ['Event A7', 'Event B7', 'Event C7', 'Event D7'],
+//      _selectedDay.add(Duration(days: 1)): ['Event A8', 'Event B8', 'Event C8', 'Event D8'],
+//      _selectedDay.add(Duration(days: 7)): ['Event A10', 'Event B10', 'Event C10'],
+//      _selectedDay.add(Duration(days: 11)): ['Event A11', 'Event B11'],
+//      _selectedDay.add(Duration(days: 22)): ['Event A13', 'Event B13'],
+//      _selectedDay.add(Duration(days: 26)): ['Event A14', 'Event B14', 'Event C14'],
     };
+
+
 
     _selectedEvents = _events[_selectedDay] ?? [];
     _calendarController = CalendarController();
