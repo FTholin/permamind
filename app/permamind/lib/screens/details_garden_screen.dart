c5402dc625a45ae22ee9e7c180b53cd372806f56
Florian THOLIN <ProteusIV@protonmail.com>
2020-02-18 16:05:00 +0100

    improve garden creation

--- app/permamind/lib/screens/details_garden_screen.dart
+++ app/permamind/lib/screens/details_garden_screen.dart
@@ -28,15 +28,15 @@ class DetailsGardenScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
-    return Scaffold(
-      appBar: GardenAppBar(gardenId: gardenId, user: user),
-      body: BlocProvider.value(
-              value: BlocProvider.of<ParcelsBloc>(context),
-              child: EnumeratedParcels(dataRepository: dataRepository, user: user)
-            ),
-      floatingActionButton: ParcelSpeedDial(
-          visible: true
-      ),
+    return BlocProvider.value(
+        value: BlocProvider.of<ParcelsBloc>(context),
+        child: Scaffold(
+          appBar: GardenAppBar(gardenId: gardenId, user: user),
+          body: EnumeratedParcels(dataRepository: dataRepository, user: user),
+          floatingActionButton: ParcelSpeedDial(
+              visible: true
+          ),
+        )
     );
   }
 }
@@ -82,116 +82,115 @@ class _GardenAppBarState extends State<GardenAppBar> {
             ),
             actions: <Widget>[
 
+              BlocBuilder<ParcelsBloc, ParcelsState>(
+                  builder: (context, state) {
+                    if (state is ParcelsLoaded) {
+                      return IconButton(
+//            tooltip: localizations.deleteGarden,
+                        // TODO ArchSampleKeys
+//                key: ArchSampleKeys.deleteGardenButton,
+                        icon: Icon(Icons.settings),
+                        onPressed: () async {
+                          List<MemberProfile> initialMember = List<
+                              MemberProfile>();
 
-//              BlocBuilder<ParcelsBloc, ParcelsState>(
-//                  builder: (context, state) {
-//                    if (state is ParcelsLoaded) {
-//                      return IconButton(
-////            tooltip: localizations.deleteGarden,
-//                        // TODO ArchSampleKeys
-////                key: ArchSampleKeys.deleteGardenButton,
-//                        icon: Icon(Icons.settings),
-//                        onPressed: () async {
-//                          List<MemberProfile> initialMember = List<
-//                              MemberProfile>();
-//
-//                          for (final member in currentGarden.members) {
-//                            if (member.id != widget.user.id) {
-//                              initialMember.add(MemberProfile(
-//                                  member.id,
-//                                  member.pseudo
-//                              ));
-//                            }
-//                          }
-//
-//
-//                          final removedGarden = await Navigator.of(context)
-//                              .push(
-//                              MaterialPageRoute(
-//                                  builder: (_) {
-//                                    return BlocProvider.value(
-//                                      value: BlocProvider.of<ActivitiesBloc>(
-//                                          context),
-//                                      child: SettingsGardenScreen(
-//                                          gardenId: currentGarden.id,
-//                                          initialMembersData: currentGarden
-//                                              .members,
-//                                          initialMember: initialMember,
-//                                          user: widget.user
-//                                      ),
-//                                    );
-//                                  })
-//                          );
-//
-//
-//                          if (removedGarden != null && removedGarden != false) {
-//                            Map returnData = Map();
-//
-////                        List<Activity> activities = List<Activity>();
-////                        state.schedule.entries.forEach((e) {
-////                          e.value.forEach((item){
-////                            activities.add(item);
-////                          });
-////                        });
-//
-//
-////                        returnData['activities'] = activities;
-//
-//
-//                            if (removedGarden['action'] == "Delete") {
-//                              returnData['garden'] = currentGarden;
-//
-//                              returnData['action'] = "Delete";
-//
-////                          BlocProvider.of<ActivitiesBloc>(context).add(DeleteActivities(currentGarden.id));
-//
-//                              BlocProvider.of<GardensBloc>(context).add(
-//                                  DeleteGarden(currentGarden));
-//
-////                          BlocProvider.of<ActivitiesBloc>(context).close();
-//
-//                              Navigator.pop(context, returnData);
-//                            } else {
-//                              if (currentGarden.members.length == 1 ||
-//                                  currentGarden.admin == widget.user.id) {
-//                                returnData['garden'] = currentGarden;
-//                                BlocProvider.of<GardensBloc>(context).add(
-//                                    DeleteGarden(currentGarden));
-//                                BlocProvider.of<ActivitiesBloc>(context).add(
-//                                    DeleteActivities(currentGarden.id));
-//                                returnData['action'] = "Delete";
-//                              } else {
-//                                returnData['action'] = "Leave";
-//
-//                                List<GardenMember> members = new List<
-//                                    GardenMember>.from(currentGarden.members);
-//
-//                                Garden copy = currentGarden.copyWith(
-//                                    name: currentGarden.name,
-//                                    id: currentGarden.id,
-//                                    publicVisibility: currentGarden
-//                                        .publicVisibility,
-//                                    admin: currentGarden.admin,
-//                                    members: members);
-//
-//                                BlocProvider.of<GardensBloc>(context).add(
-//                                    LeaveGarden(currentGarden, widget.user.id)
-//                                );
-//
-//                                returnData['garden'] = copy;
-//                              }
-//
-//                              BlocProvider.of<ActivitiesBloc>(context).close();
-//
-//                              Navigator.pop(context, returnData);
-//                            }
-//                          }
-//                        },
-//                      );
-//                    } else {
-//                      return Container();
-//                    }
-//                  })
+                          for (final member in currentGarden.members) {
+                            if (member.id != widget.user.id) {
+                              initialMember.add(MemberProfile(
+                                  member.id,
+                                  member.pseudo
+                              ));
+                            }
+                          }
+
+
+                          final removedGarden = await Navigator.of(context)
+                              .push(
+                              MaterialPageRoute(
+                                  builder: (_) {
+                                    return BlocProvider.value(
+                                      value: BlocProvider.of<ParcelsBloc>(
+                                          context),
+                                      child: SettingsGardenScreen(
+                                          gardenId: currentGarden.id,
+                                          initialMembersData: currentGarden
+                                              .members,
+                                          initialMember: initialMember,
+                                          user: widget.user
+                                      ),
+                                    );
+                                  })
+                          );
+
+
+                          if (removedGarden != null && removedGarden != false) {
+                            Map returnData = Map();
+
+//                        List<Activity> activities = List<Activity>();
+//                        state.schedule.entries.forEach((e) {
+//                          e.value.forEach((item){
+//                            activities.add(item);
+//                          });
+//                        });
+
+
+//                        returnData['activities'] = activities;
+
+
+                            if (removedGarden['action'] == "Delete") {
+                              returnData['garden'] = currentGarden;
+
+                              returnData['action'] = "Delete";
+
+//                          BlocProvider.of<ActivitiesBloc>(context).add(DeleteActivities(currentGarden.id));
+
+                              BlocProvider.of<GardensBloc>(context).add(
+                                  DeleteGarden(currentGarden));
+
+//                          BlocProvider.of<ActivitiesBloc>(context).close();
+
+                              Navigator.pop(context, returnData);
+                            } else {
+                              if (currentGarden.members.length == 1 ||
+                                  currentGarden.admin == widget.user.id) {
+                                returnData['garden'] = currentGarden;
+                                BlocProvider.of<GardensBloc>(context).add(
+                                    DeleteGarden(currentGarden));
+                                BlocProvider.of<ActivitiesBloc>(context).add(
+                                    DeleteActivities(currentGarden.id));
+                                returnData['action'] = "Delete";
+                              } else {
+                                returnData['action'] = "Leave";
+
+                                List<GardenMember> members = new List<
+                                    GardenMember>.from(currentGarden.members);
+
+                                Garden copy = currentGarden.copyWith(
+                                    name: currentGarden.name,
+                                    id: currentGarden.id,
+                                    publicVisibility: currentGarden
+                                        .publicVisibility,
+                                    admin: currentGarden.admin,
+                                    members: members);
+
+                                BlocProvider.of<GardensBloc>(context).add(
+                                    LeaveGarden(currentGarden, widget.user.id)
+                                );
+
+                                returnData['garden'] = copy;
+                              }
+
+                              BlocProvider.of<ActivitiesBloc>(context).close();
+
+                              Navigator.pop(context, returnData);
+                            }
+                          }
+                        },
+                      );
+                    } else {
+                      return Container();
+                    }
+                  })
 
             ],
           );
